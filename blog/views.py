917192e76ec96ab1d87623b59e7fdbaea501b3bd
eigenlambda123 <rmvilla987@gmail.com>
2025-02-27 00:47:13 +0800

    Initialize blog application with basic structure and Implemented Sending Message Through GMAIL, Added Features such as Comments, Pagination and Tags

---
+++ blog/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage  
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.post = post
+        new_comment.save()
+        return redirect(post.get_absolute_url())
+
+    comments = post.comments.filter(active=True)
+    return render(request, 'blog/post/detail.html', {'post': post, 'form': form, 'comment': comment})
+
+
+def post_list(request, tag_slug=None):
+    posts = Post.published_manager.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        posts = posts.filter(tags__in=[tag])
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+
+
+
+def post_detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, 
+                            slug=slug, status=Post.Status.PUBLISHED,
+                            published__year=year,
+                            published__month=month,
+                            published__day=day)
+
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'eigenlambda123@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+
+
+# class PostListView(ListView):
+#     queryset = Post.published_manager.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
