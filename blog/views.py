ef95fd7ff7108a9b37d0513fcd336bc9674e3afd
eigenlambda123 <rmvilla987@gmail.com>
2025-02-27 11:45:48 +0800

    Add similar posts feature and fix the pagination

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 @require_POST
 def post_comment(request, post_id):
@@ -36,8 +37,7 @@ def post_list(request, tag_slug=None):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
-
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page, 'tag': tag, 'page_obj': posts})
 
 
 def post_detail(request, year, month, day, slug):
@@ -47,9 +47,15 @@ def post_detail(request, year, month, day, slug):
                             published__month=month,
                             published__day=day)
 
+    # list of active comments for this post
     comments = post.comments.filter(active=True)
+    # form for adding comments
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published_manager.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-published')[:4]
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
