b4d26faa09fe8a21f5ca40d6d01db5ecfb9cdf9a
eigenlambda123 <rmvilla987@gmail.com>
2025-03-01 15:42:03 +0800

    Add search functionality for blog posts

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage  
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 @require_POST
 def post_comment(request, post_id):
@@ -74,6 +75,19 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form})
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published_manager.annotate(
+                similarity=TrigramSimilarity('title', query) + TrigramSimilarity('body', query)
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
 
 # class PostListView(ListView):
